25e215ffc128cc8ffc38019b5065cc641c3efc3f
Ryan Eloy <math.eloy@hotmail.com>
2024-02-28 02:38:34 -0300

    chore:(Metadata): complete metadatas

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,17 +7,21 @@ import { Roboto as FontSans, Roboto_Mono as FontMono } from "next/font/google"
 import { ThemeProvider } from "@/components/theme-provider"
 import LanguageProvider from "@/context/language-provider"
 
+import linkedinPhoto from '@/public/linkedin-photo.jpeg'
+
 export const metadata: Metadata = {
   title: 'Ryan Eloy | Portfolio',
-  description: '',
+  description: 'Passionate about technology with 5-6 years of experience creating scalable and efficient web applications. Driven by challenges!',
   authors: [{
     name: 'Ryan Eloy', url: 'www.linkedin.com/in/ryan-eloy-5906b91a5'
   }],
   openGraph: {
     title: 'Ryan Eloy | Portfolio',
-    description: '',
-    images: []
-  }
+    description: 'Passionate about technology with 5-6 years of experience creating scalable and efficient web applications. Driven by challenges!',
+    images: [linkedinPhoto.src]
+  },
+  category: 'Web Development',
+  robots: 'index'
 }
 
 const fontSans = FontSans({
